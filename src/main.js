1e56cbd15f8948a6cac022814b9b424c78a42475
Matthias Hryniszak <padcom@gmail.com>
2021-03-28 04:09:08 +0200

    Added logging to console

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import { createApp } from 'vue'
 import App from './App'
 import './main.scss'
-
 import router from './router'
+
+import { Logger, LogLevel } from './logger'
+
+Logger.events.on('trace', ({ source, args }) => { console.log(`[${source}]`, ...args) })
+Logger.events.on('debug', ({ source, args }) => { console.debug(`[${source}]`, ...args) })
+Logger.events.on('info', ({ source, args }) => { console.info(`[${source}]`, ...args) })
+Logger.events.on('warn', ({ source, args }) => { console.warn(`[${source}]`, ...args) })
+Logger.events.on('error', ({ source, args }) => { console.error(`[${source}]`, ...args) })
+
+Logger.getLogger('MSPv1').level = LogLevel.trace
+Logger.getLogger('MSPv2').level = LogLevel.trace
 
 const app = createApp(App).use(router)
 
