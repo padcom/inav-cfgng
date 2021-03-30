20579a4e570378e3d0545b05a6cf6fe644c50046
Matthias Hryniszak <mhryn@softserveinc.com>
2021-03-30 11:26:00 +0200

    Code cleanup

--- src/composables/registry.js
+++
@@ -1,8 +0,0 @@
-import { ref } from 'vue'
-import { registry } from '../registry'
-
-const registryRef = ref(registry)
-
-export function useCommandRegistry() {
-  return { registry: registryRef }
-}
