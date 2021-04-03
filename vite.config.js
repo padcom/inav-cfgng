5d6fc807bcb94365ce334534e5b074dd1de4b8bd
Matthias Hryniszak <mhryn@softserveinc.com>
2021-04-03 11:42:06 +0200

    Enabled building final version of the application

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import vue from '@vitejs/plugin-vue'
+import acornPrivateMethods from 'acorn-private-methods'
 
 export default {
+  build: {
+    rollupOptions: {
+      acornInjectPlugins: [ acornPrivateMethods ]
+    }
+  },
   plugins: [
     vue()
   ]
